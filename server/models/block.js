a838725c57adbd4ca7cd50b7739f9328f59e36b3
David Meir-Levy <davidmeirlevy@gmail.com>
2020-11-27 21:55:32 +0200

    fix block model

--- server/models/block.js
+++ server/models/block.js
@@ -1,73 +1,60 @@
-const shortId = require("shortid");
 const mongoose = require("mongoose");
 const cacheManager = require("../utils/cache-manager");
 
-const cachePrefix = "block:";
+const searchedLayouts = { post: 'relatedPosts', category: 'relatedCategories' };
 
 const BlockSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  tenant: {
-    type: String,
-    required: true,
-  },
-  description: { //editors only in controller!
-    type: String,
-  },
-  contents: [],
-  contentType: {
-    type: String,
-    enum: [
-      "content", // for regular blocks
-      "images", // for a gallery of images
-      "script", // for ads
-    ],
-  },
-  relatedCategories: [{
-		type: mongoose.Schema.Types.ObjectID, 
-		ref: category
-	}],
-  relatedPosts: [{
-		type: mongoose.Schema.Types.ObjectID, 
-		ref: post
-	}],
-  relatedLayouts: [{
+	name: {
 		type: String,
-		enum: ['layout', 'index', 'category', 'post', 'search', 'tag', 'error']
-	  }],
+		required: true,
+	},
+	tenant: {
+		type: String,
+		required: true,
+	},
+	description: { //editors only in controller!
+		type: String,
+	},
+	contents: [],
+	contentType: {
+		type: String,
+		enum: [
+			"content", // for regular blocks
+			"images", // for a gallery of images
+			"script", // for ads
+		],
+	},
+	relatedCategories: [ {
+		type: mongoose.Schema.Types.ObjectID,
+		ref: 'category'
+	} ],
+	relatedPosts: [ {
+		type: mongoose.Schema.Types.ObjectID,
+		ref: 'post'
+	} ],
+	relatedLayouts: [ {
+		type: String,
+		enum: [ 'layout', 'index', 'category', 'post', 'search', 'tag', 'error' ]
+	} ],
 });
 
-// some useful methods
-
-BlockSchema.statics.Search = function Search(
-  query = {},
-  { limit, offset },
-) {
-  return this.find(query)
-    .limit(limit)
-    .offset(offset)
-    .then((list) => {
-      if (list && list.length) return JSON.stringify(list);
-      else return "[]";
-    });
+BlockSchema.statics.search = function search(relatedLayout, itemId) {
+	const query = {
+		relatedLayouts: relatedLayout
+	};
+	if (searchedLayouts[relatedLayout]) {
+		query[searchedLayouts[relatedLayout]] = itemId;
+	}
+	return this.find(query).lean().select('-description -tenant');
 };
 
-BlockSchema.statics.SingleBlock = function SingleBlock(
-  blockId,
-  tenant,
-  useCache = false,
-) {
-  const cacheString = `${cachePrefix}single:${blockId}.${tenant}`;
-
-  const q = () => this.findOne({ _id: blockId, tenant: tenant });
-
-  if (useCache) {
-    return cacheManager.wrap(cacheString, q);
-  } else {
-    return q();
-  }
+BlockSchema.statics.getLayoutBlocks = function getLayoutBlocks() {
+	return this
+		.find({
+			relatedLayouts: 'layout'
+		})
+		.lean()
+		.select('-description -tenant');
 };
 
-module.exports = mongoose.model("Block", BlockSchema);
+module.exports = mongoose.model('Block', BlockSchema);
